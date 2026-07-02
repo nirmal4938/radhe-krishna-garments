adcd0b4b28b493cb396c1ae197624ac496d2c2ae
nirmalkj2025 <nirmal@kayjayglobal.com>
2026-07-03 00:20:19 +0530

    (feat) date 02-07-2026 milestone achieved

--- src/services/storefront.service.js
+++ src/services/storefront.service.js
@@ -3,7 +3,11 @@ import ENDPOINTS from "../api/endpoints";
 
 class StorefrontService {
   async bootstrap() {
-    const { data } = await api.get(ENDPOINTS.STORE.BOOTSTRAP);
+    const { data } = await api.get(ENDPOINTS.STORE.BOOTSTRAP, {
+      headers: {
+        "X-Tenant-Host": window.location.hostname,
+      },
+    });
 
     return data;
   }
